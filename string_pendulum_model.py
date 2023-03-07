bff46c8c5aaca9e12d06ae4db5667ea069b6a061
billlllllllly <102607513+billlllllllly@users.noreply.github.com>
2023-03-07 22:30:30 +0800

    Update string_pendulum_model.py

--- string_pendulum_model.py
+++ string_pendulum_model.py
@@ -3,36 +3,51 @@ from PIL import Image
 from math import *
 
 zoominrate = 1
-k = float(0) #N/m
+k = float(100) #N/m
 m = float(1) #kg
-dt = 0.1   #s
+dt = 0.001   #s
 x0, y0 = 100, 25
-x = float(150)#cm
-y = float(125)#cm
+x = float(100+40)#cm
+y = float(0+30)#cm
 g = 9.8 #m/s2
-l0 = 0.10 #m
-vx = -20.0 #m/s
+l0 = 20 #cm
+vx = 0.0 #m/s
 ax = 0.0 #m/s2
-vy = 20.0 #m/s
+vy = 0.0 #m/s
 ay = 0.0 #m/s2
 t = 0.0  #s
+sizex, sizey = 200, 200
 
-p1 = PIL.Image.new(mode="RGB", size=(200, 400), color = (250, 250, 250))
+
+p1 = PIL.Image.new(mode="RGB", size=(sizex, sizey), color = (250, 250, 250))
 p2 = p1.load()
 
 def length(x, y):
-    return(sqrt((x-x0)**2 + (y - y0)**2))
+    temp = sqrt((x-x0)**2 + (y - y0)**2)
+    #print(temp)
+    return(temp)
 
-while t <= 10:
+p2[x-1, y-1] = (200, 0, 0)
+p2[x-1, y] = (200, 0, 0)
+p2[x-1, y+1] = (200, 0, 0)
+p2[x, y-1] = (200, 0, 0)
+p2[x, y] = (200, 0, 0)
+p2[x, y+1] = (200, 0, 0)
+p2[x+1, y-1] = (200, 0, 0)
+p2[x+1, y] = (200, 0, 0)
+p2[x+1, y+1] = (200, 0, 0)
+
+while t <= 50:
     l = length(x, y)
     x  += vx*dt + ax*dt*dt/2
     y  += vy*dt + ay*dt*dt/2
-    ax += (k*(l - l0)/100*(50-x)/l)/m
-    ay += (k*(l - l0)/100*(0-y)/l + m*g)/m
+    ax = (k*(l - l0)/100*(50-x)/l)/m
+    ay = (k*(l - l0)/100*(0-y)/l + m*g)/m
     vx += ax*dt
     vy += ay*dt
+    #print(f"[{0}{1}]", ax, ay)
     
-    if floor(x)<200 and floor(y)<200 and floor(x)>0 and floor(y)>0:
+    if floor(x)<sizex and floor(y)<sizey and floor(x)>0 and floor(y)>0:
         p2[floor(x), floor(y)] = (0, 0, 0)
     t += dt
 
