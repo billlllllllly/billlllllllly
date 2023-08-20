6eaed3a806779f3b170d6e17b8ab1d0c30a12fa3
billlllllllly <102607513+billlllllllly@users.noreply.github.com>
2023-08-21 00:25:03 +0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,14 +1,19 @@
 inputfile = open("VocabInput.txt", "r", encoding='utf-8')
-vocablistfile = open("VocabM4-12.txt", "w", encoding='utf-8')
+vocablistfile = open("temp.txt", "w", encoding='utf-8')
 
 inputfiledata = inputfile.readlines()
 for i in range(len(inputfiledata)):
-    vocablistfile.write("{\"English\":\"")
-    vocablistfile.write(inputfiledata[i].split()[0])
-    vocablistfile.write("\", \"Chinese\":\"")
-    vocablistfile.write(inputfiledata[i].split()[1])
-    vocablistfile.write("\", \"weight\":2}\n")
+    word = inputfiledata[i].split()
 
+    vocablistfile.write("{\"English\":\"")
+    for j in range(len(word)-2):
+        vocablistfile.write(word[j]+' ')
+    vocablistfile.write(word[len(word)-2])
+
+    vocablistfile.write("\", \"Chinese\":\"")
+    vocablistfile.write(word[len(word)-1])
+
+    vocablistfile.write("\", \"weight\":2}\n")
 
 inputfile.close()
 vocablistfile.close()
