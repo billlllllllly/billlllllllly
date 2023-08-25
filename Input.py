56a698b2b1685b939ebadceb60343245e5ee256a
billlllllllly <102607513+billlllllllly@users.noreply.github.com>
2023-08-25 21:16:49 +0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,16 +1,30 @@
+import re
+
+def getchidx(intxt):
+    for i in range(len(intxt)):
+        x = re.findall("[a-zA-Z()0-9\'\"\/.~\[\]]", intxt[i])
+        if len(intxt[i]) != len(x):
+            print(i)
+            return i
+
+
 inputfile = open("VocabInput.txt", "r", encoding='utf-8')
 vocablistfile = open("temp.txt", "w", encoding='utf-8')
 
 inputfiledata = inputfile.readlines()
 for i in range(len(inputfiledata)):
     word = inputfiledata[i].split()
+    print(word , end='  ')
+    chidx = getchidx(word)
 
     vocablistfile.write("{\"English\":\"")
-    for j in range(len(word)-2):
+    for j in range(chidx-1):
         vocablistfile.write(word[j]+' ')
-    vocablistfile.write(word[len(word)-2])
+    vocablistfile.write(word[chidx-1])
 
     vocablistfile.write("\", \"Chinese\":\"")
+    for j in range(chidx, len(word)-1):
+        vocablistfile.write(word[j]+' ')
     vocablistfile.write(word[len(word)-1])
 
     vocablistfile.write("\", \"weight\":2}\n")
