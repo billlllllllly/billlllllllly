bd4aa7912fd32e86652b47ca640de02216e1fdfe
billlllllllly <102607513+billlllllllly@users.noreply.github.com>
2024-03-12 21:40:12 +0800

    Add files via upload

---
+++ ElasticPendulumSimulation/dataset.py
@@ -0,0 +1,22 @@
+from torch.utils.data.dataset import Dataset
+from torch import tensor
+import pandas as pd
+import numpy as np
+
+
+class EPSdataset(Dataset):
+    def __init__(self, root_dir, csv_file, transfrom=None):
+        super().__init__()
+        self.root_dir = root_dir
+        self.annotation = pd.read_csv(csv_file)
+        self.transform = transfrom
+
+    def __len__(self):
+        return len(self.annotation)
+
+    def __getitem__(self, index):
+        sample = np.array(self.annotation.iloc[index])
+        X = sample[:-1]
+        y = sample[-1:]
+        return tensor(X), tensor(y)
+    
